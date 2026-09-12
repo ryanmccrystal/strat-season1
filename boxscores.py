08c3769938be69388b4d2d2726ffd06c1b34abe0
Ryan McCrystal <30356426+ryanmccrystal@users.noreply.github.com>
2026-09-12 11:27:16 -0400

    Update SPREADSHEET_ID to new value

--- boxscores.py
+++ boxscores.py
@@ -5,7 +5,7 @@ import csv
 import gspread
 from google.oauth2.service_account import Credentials
 
-SPREADSHEET_ID = "1hPnUsWFFjbFQZPrqc2F4X9f4ytjP2zb9sdhp8T0gjN0"
+SPREADSHEET_ID = "1NkwN2hKQGNRcSdfAyFTyAMvp7I29CxmLsw-LXjCwT5A"
 
 TEAM_NAMES = {
     "PDX": "Portland",
